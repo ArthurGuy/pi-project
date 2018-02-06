896523c9c023c20b8be9cef92280fd41a4922a61
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 17:45:05 +0000

    Create temp-speak.py

---
+++ temp-speak.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import aiy.voicehat
+
+#Ignore warnings about redefining the GPIO port
+GPIO.setwarnings(False)
+
+led = aiy.voicehat.get_led()
+button = aiy.voicehat.get_button()
+audio = aiy.audio
+
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT22
+pin = 26
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+if humidity is not None and temperature is not None:
+  audio.say('The temperature is {0:0.1f}*C'.format(temperature, humidity))
