3300e8033a4a80f0ecfb52dab0018b408b6f94b9
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 22:10:11 +0000

    Update camera-test.py

--- camera-test.py
+++ camera-test.py
@@ -16,7 +16,7 @@ button = aiy.voicehat.get_button()
 
 preview_mode = False
 
-app = App(title="Camera")
+app = App(title="Camera", height=768, width=1024)
 
 img = ImageTk.PhotoImage(Image.open('test.jpg'))
 preview = Text(app, text="Preview")
@@ -28,6 +28,7 @@ def button_take_photo():
     if preview_mode == True:
         led.set_state(aiy.voicehat.LED.BLINK_3)
         time.sleep(2)
+        camera.resolution = (1024, 768)
         camera.capture('test.jpg')
         led.set_state(aiy.voicehat.LED.PULSE_SLOW)
         camera.stop_preview()
@@ -48,18 +49,10 @@ try:
     led.set_state(aiy.voicehat.LED.PULSE_SLOW)
     
     welcome_message = Text(app, text="Photo booth")
-    #photo = Picture(app, image="test.jpg")
     
     
     button.on_press(button_take_photo)
-    
-    
-    #button.wait_for_press()
-    #led.set_state(aiy.voicehat.LED.BLINK_3)
-    #time.sleep(2)
-    #camera.capture('test.jpg')
-    
-    #camera.stop_preview()
+
     
     app.display()
     
