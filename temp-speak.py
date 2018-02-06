66fb5947a257c2707b0b19c733caca6401886360
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 17:53:38 +0000

    Update temp-speak.py

--- temp-speak.py
+++ temp-speak.py
@@ -16,4 +16,4 @@ pin = 26
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
-  audio.say('The temperature is {0:0.1f} degrees and the humidity is {1:f}%'.format(temperature, trunc(humidity)), lang="en-GB", volume=75, pitch=135)
+  audio.say('The temperature is {0:0.1f} degrees and the humidity is {1}%'.format(temperature, trunc(humidity)), lang="en-GB", volume=75, pitch=135)
