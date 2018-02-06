1b50228f2376a949e11050ec935063eda3dc71d7
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 11:35:25 +0000

    Create led-test.py

---
+++ led-test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import aiy._drivers._led
+
+# GPIO definitions (BCM)
+_GPIO_BUTTON = 23
+_GPIO_LED = 25
+
+# Import LED class to expose the LED constants.
+LED = aiy._drivers._led.LED
+
+ _voicehat_led = aiy._drivers._led.LED(channel=_GPIO_LED)
+ _voicehat_led.start()
+ 
+ led.set_state(aiy.voicehat.LED.BLINK)
