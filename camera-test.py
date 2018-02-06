f4832fc57fdd4dbdb58d19566467d150915894ac
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 21:20:05 +0000

    Update camera-test.py

--- camera-test.py
+++ camera-test.py
@@ -2,12 +2,18 @@ import time
 import picamera
 import RPi.GPIO as GPIO
 import aiy.voicehat
+from guizero import App, Text, Picture
 
 #Ignore warnings about redefining the GPIO port
 GPIO.setwarnings(False)
 
 led = aiy.voicehat.get_led()
 button = aiy.voicehat.get_button()
+
+app = App(title="Camera")
+
+welcome_message = Text(app, text="Photo booth")
+app.display()
 
 camera = picamera.PiCamera()
 try:
@@ -18,5 +24,8 @@ try:
     time.sleep(2)
     camera.capture('test.jpg')
     camera.stop_preview()
+    
+    my_cat = Picture(app, image="test.jpg")
+    
 finally:
     camera.close()
