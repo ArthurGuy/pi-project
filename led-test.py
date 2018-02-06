2c0520fa9cfb6b833312212b6e83a260d94fbf28
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 13:26:56 +0000

    Update led-test.py

--- led-test.py
+++ led-test.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python
 
 import aiy.voicehat
-import aiy._drivers._led
 
-# GPIO definitions (BCM)
-_GPIO_BUTTON = 23
-_GPIO_LED = 25
+led = aiy.voicehat.get_led()
 
-# Import LED class to expose the LED constants.
-LED = aiy._drivers._led.LED
-
-_voicehat_led = aiy._drivers._led.LED(channel=_GPIO_LED)
-_voicehat_led.start()
-
-_voicehat_led.set_state(aiy.voicehat.LED.BLINK)
+led.set_state(aiy.voicehat.LED.BLINK)
