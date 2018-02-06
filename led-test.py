a93ea231bc07a9bd548a7e7099f4d79bf444a8cf
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 13:33:59 +0000

    Update led-test.py

--- led-test.py
+++ led-test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
+import RPi.GPIO as GPIO
 import aiy.voicehat
 
 led = aiy.voicehat.get_led()
+button = aiy.voicehat.get_button()
 
-led.set_state(aiy.voicehat.LED.BLINK)
+led.set_state(aiy.voicehat.LED.PULSE_QUICK)
+
+button.wait_for_press()
