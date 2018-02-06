8146d4107791f99c577dd6d5db7a2a915fd61074
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 21:30:57 +0000

    Update camera-test.py

--- camera-test.py
+++ camera-test.py
@@ -4,28 +4,45 @@ import RPi.GPIO as GPIO
 import aiy.voicehat
 from guizero import App, Text, Picture
 
+# export DISPLAY=:0.0
+
 #Ignore warnings about redefining the GPIO port
 GPIO.setwarnings(False)
 
 led = aiy.voicehat.get_led()
 button = aiy.voicehat.get_button()
 
-app = App(title="Camera")
 
-welcome_message = Text(app, text="Photo booth")
-app.display()
+def button_take_photo(_):
+    led.set_state(aiy.voicehat.LED.BLINK_3)
+    time.sleep(2)
+    camera.capture('test.jpg')
+    led.set_state(aiy.voicehat.LED.PULSE_SLOW)
+
+
+app = App(title="Camera")
 
 camera = picamera.PiCamera()
 try:
     camera.start_preview()
-    led.set_state(aiy.voicehat.LED.PULSE_SLOW)
-    button.wait_for_press()
-    led.set_state(aiy.voicehat.LED.BLINK_3)
-    time.sleep(2)
-    camera.capture('test.jpg')
-    camera.stop_preview()
+    #led.set_state(aiy.voicehat.LED.PULSE_SLOW)
     
-    my_cat = Picture(app, image="test.jpg")
+    welcome_message = Text(app, text="Photo booth")
+    photo = Picture(app, image="test.jpg")
+    
+    
+    button.on_press(button_take_photo)
+    
+    
+    #button.wait_for_press()
+    #led.set_state(aiy.voicehat.LED.BLINK_3)
+    #time.sleep(2)
+    #camera.capture('test.jpg')
+    
+    #camera.stop_preview()
+    photo.image = 'test.jpg'
+    
+    app.display()
     
 finally:
     camera.close()
