5731cefccd984f399c8c6ba43bf00243d1d8c340
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-06 11:35:51 +0000

    Update led-test.py

--- led-test.py
+++ led-test.py
@@ -9,7 +9,7 @@ _GPIO_LED = 25
 # Import LED class to expose the LED constants.
 LED = aiy._drivers._led.LED
 
- _voicehat_led = aiy._drivers._led.LED(channel=_GPIO_LED)
- _voicehat_led.start()
- 
- led.set_state(aiy.voicehat.LED.BLINK)
+_voicehat_led = aiy._drivers._led.LED(channel=_GPIO_LED)
+_voicehat_led.start()
+
+led.set_state(aiy.voicehat.LED.BLINK)
