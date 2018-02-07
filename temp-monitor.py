ef39c68ae4e90f0acb2b1e328b3760eab6ed961b
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-07 18:53:28 +0000

    Update temp-monitor.py

--- temp-monitor.py
+++ temp-monitor.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+
 sensor = Adafruit_DHT.DHT22
 pin = 26
+
+fan_pin = 04
+GPIO.setup(fan_pin, GPIO.OUT)
+
+GPIO.output(fan_pin, True)
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
@@ -14,3 +21,5 @@ if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
+
+GPIO.output(fan_pin, False) 
