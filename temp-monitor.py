2049475639168db912998079baa5aae93157a620
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-07 19:01:48 +0000

    Update temp-monitor.py

--- temp-monitor.py
+++ temp-monitor.py
@@ -2,6 +2,7 @@
 
 import Adafruit_DHT
 import RPi.GPIO as GPIO
+import time
 
 sensor = Adafruit_DHT.DHT22
 pin = 26
@@ -10,20 +11,18 @@ fan_pin = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(fan_pin, GPIO.OUT)
 
+while True:
 
-
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     
-if temperature > 20:
-    GPIO.output(fan_pin, True)
-else:
-    GPIO.output(fan_pin, False) 
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
+    if temperature > 20:
+        GPIO.output(fan_pin, True)
+    else:
+        GPIO.output(fan_pin, False) 
+
+    time.sleep(15)
