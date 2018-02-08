aeb79726b9aa30ede469032a34a8849759fdf8f8
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-08 09:22:43 +0000

    Update temp-monitor.py

--- temp-monitor.py
+++ temp-monitor.py
@@ -4,26 +4,34 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 
-sensor = Adafruit_DHT.DHT22
-pin = 26
+sensor_pin = 26
+fan_pin = 4
+
 max_temp = 20
 
-fan_pin = 4
+sensor = Adafruit_DHT.DHT22
+
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(fan_pin, GPIO.OUT)
 
-while True:
+try:
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
 
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    
-    if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-    else:
-        print('Failed to get reading. Try again!')
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading.')
 
-    if temperature > max_temp:
-        GPIO.output(fan_pin, True)
-    elif temperature < (max_temp - 1):
-        GPIO.output(fan_pin, False) 
+        if temperature is not None
+            if temperature > max_temp:
+                GPIO.output(fan_pin, True)
+            elif temperature < (max_temp - 1):
+                GPIO.output(fan_pin, False) 
 
-    time.sleep(15)
+        time.sleep(15)
+        
+except KeyboardInterrupt:
+    GPIO.output(fan_pin, False) 
+    print('Exiting')
