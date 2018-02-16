f513ac18099538c732da83bd5b737bbfaea78b37
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-16 14:10:04 +0000

    Update camera-test.py

--- camera-test.py
+++ camera-test.py
@@ -1,16 +1,7 @@
 import time
 import picamera
-import RPi.GPIO as GPIO
-import aiy.voicehat
 
 # export DISPLAY=:0.0
-
-#Ignore warnings about redefining the GPIO port
-GPIO.setwarnings(False)
-
-led = aiy.voicehat.get_led()
-button = aiy.voicehat.get_button()
-
 
 
 while True:
