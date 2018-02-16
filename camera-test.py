d7cceb986bb7e7b60bbd982f7fc9f847054cff6b
Arthur Guy <arthur@arthurguy.co.uk>
2018-02-16 14:35:33 +0000

    Update camera-test.py

--- camera-test.py
+++ camera-test.py
@@ -3,16 +3,15 @@ import picamera
 
 # export DISPLAY=:0.0
 
+camera = picamera.PiCamera()
+camera.rotation = 0
+camera.preview_fullscreen = False
+
+camera.resolution = (640, 360)
+camera.start_preview()
+camera.preview.window = (192, 100, 640, 360)
 
 while True:
-    
-    camera = picamera.PiCamera()
-    camera.rotation = 0
-    camera.preview_fullscreen = False
-
-    camera.resolution = (640, 360)
-    camera.start_preview()
-    camera.preview.window = (192, 100, 640, 360)
     
 
 camera.close()
